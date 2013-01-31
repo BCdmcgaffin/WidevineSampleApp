7ded638a306fbcecdeb448ac846ccfd501df1650
David McGaffin <dmcgaffin@brightcove.com>
2013-01-31 18:29:00 -0500

    Initial Commit

---
+++ WidevineSampleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WidevineSampleApp
+//
+//  Created by David McGaffin on 1/31/13.
+//  Copyright (c) 2013 Brightcove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
